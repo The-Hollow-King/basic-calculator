9d7c984cdf8d6676aaac3f869facadf16633da6e
Kiran Balam <kiranbalam25@gmail.com>
2026-07-26 22:33:29 +0530

    Refactor basic calculator for clarity and structure
    Added comments for easier understanding

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,8 +1,10 @@
 #basic calculator
-import sys
-print("Welcome to basic calculator program")
+import sys #to invoke sys.exit() to end the program later on
+print("Welcome to basic calculator program") #welcome statement
+#creating two variables to accept and store numbers which would be entered by the user for the operation which they want to perform
 a = int(input("Enter the first number you want to use for an operation :"))
 b = int(input("Enter the second number :"))
+#creating a variable to accept the respective sign of the operation which the user wants to perform
 q = input("Input the sign of the operator you want to use for the operation(+ , - , / , * , // , %):")
 if(q == "+" or q == "-") or (q == "/" or q == "*") or (q == "//" or q == "%"):
  print ("Appropriate operator is used")
@@ -24,6 +26,6 @@ elif(q == "//") :
 elif(q == "%") :
  c = a % b
  print ("Answer :", c)
-else :
- print("Unidentified operator/character, reset the program")
- sys.exit()
+else : 
+ print("Unidentified operator/character, reset the program") #for when user inputs an unmentioned sign or something unnecesary
+ sys.exit() #to stop the program
